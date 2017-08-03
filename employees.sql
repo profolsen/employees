d4f153f9b314f974ca17da600cb1b1c53b34cb8a
profolsen <profolsen@users.noreply.github.com>
2017-08-03 10:39:28 -0400

    removing some code

--- employees.sql
+++ employees.sql
@@ -8,14 +8,11 @@
 --  Any similarity to existing people is purely coincidental.
 -- 
 
-CREATE TYPE gender AS ENUM('M', 'F');
-
 CREATE TABLE employees (
     emp_no      INT             NOT NULL,
     birth_date  DATE            NOT NULL,
     first_name  VARCHAR(14)     NOT NULL,
     last_name   VARCHAR(16)     NOT NULL,
-    gender      gender 		NULL,    
     hire_date   DATE            NOT NULL,
     PRIMARY KEY (emp_no)
 );
