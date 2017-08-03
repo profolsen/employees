6bcdd104af829f1890c4456681e2579dfa6e7a06
profolsen <olsenp@strose.edu>
2017-08-03 14:08:20 -0400

    up through managers of departments.

--- sql/employees.sql
+++ sql/employees.sql
@@ -19,10 +19,10 @@ DROP TABLE IF EXISTS employees;
 
 CREATE TABLE employees (
     emp_no      INT             NOT NULL,
-    birth_date  DATE            NOT NULL,
+    birth_date  TIMESTAMP       NOT NULL,
     first_name  VARCHAR(14)     NOT NULL,
     last_name   VARCHAR(16)     NOT NULL,
-    hire_date   DATE            NOT NULL,
+    hire_date   TIMESTAMP       NOT NULL,
     PRIMARY KEY (emp_no)
 );
 
@@ -36,8 +36,8 @@ CREATE TABLE departments (
 CREATE TABLE dept_manager (
    dept_no      CHAR(4)         NOT NULL,
    emp_no       INT             NOT NULL,
-   from_date    DATE            NOT NULL,
-   to_date      DATE            NOT NULL,
+   from_date    TIMESTAMP       NOT NULL,
+   to_date      TIMESTAMP       ,
    FOREIGN KEY (emp_no)  REFERENCES employees (emp_no)    ON DELETE CASCADE,
    FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
    PRIMARY KEY (emp_no,dept_no)
@@ -46,8 +46,8 @@ CREATE TABLE dept_manager (
 CREATE TABLE works_in (
     emp_no      INT             NOT NULL,
     dept_no     CHAR(4)         NOT NULL,
-    from_date   DATE            NOT NULL,
-    to_date     DATE            NOT NULL,
+    from_date   TIMESTAMP       NOT NULL,
+    to_date     TIMESTAMP       ,
     FOREIGN KEY (emp_no)  REFERENCES employees   (emp_no)  ON DELETE CASCADE,
     FOREIGN KEY (dept_no) REFERENCES departments (dept_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no,dept_no)
@@ -56,8 +56,8 @@ CREATE TABLE works_in (
 CREATE TABLE titles (
     emp_no      INT             NOT NULL,
     title       VARCHAR(50)     NOT NULL,
-    from_date   DATE            NOT NULL,
-    to_date     DATE,
+    from_date   TIMESTAMP       NOT NULL,
+    to_date     TIMESTAMP,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no,title, from_date)
 ); 
@@ -66,8 +66,109 @@ CREATE TABLE titles (
 CREATE TABLE salaries (
     emp_no      INT             NOT NULL,
     salary      INT             NOT NULL,
-    from_date   DATE            NOT NULL,
-    to_date     DATE            NOT NULL,
+    from_date   TIMESTAMP       NOT NULL,
+    to_date     TIMESTAMP       ,
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
     PRIMARY KEY (emp_no, from_date)
 );
+
+DROP TABLE IF EXISTS firstnames;
+CREATE TABLE firstnames (
+    firstname VARCHAR(14),
+    id INT
+);
+
+DROP TABLE IF EXISTS lastnames;
+CREATE TABLE lastnames (
+    lastname VARCHAR(16),
+    id INT
+);
+INSERT INTO firstnames VALUES
+    ('Alice', 0),
+    ('Bob', 1),
+    ('Charlotte', 3),
+    ('Dagwood', 4),
+    ('Edward', 5),
+    ('Felicity', 6),
+    ('Garry', 7),
+    ('Heather', 8),
+    ('Ivan', 9),
+    ('Jada', 10),
+    ('Paul', 2),
+    ('Quentin', 11),
+    ('Rachel', 12);
+
+INSERT INTO lastnames VALUES
+    ('Karenin', 0),
+    ('Levin', 1),
+    ('Mirsky', 2),
+    ('Karamozov', 4),
+    ('Scherbatsky', 5),
+    ('Tolstoy', 3),
+    ('DeLillo', 6);
+
+INSERT INTO employees
+    SELECT generate_series as emp_no,
+           ((1980 + generate_series % 37) || '-' || ((generate_series % 12) + 1) || '-' || ((generate_series % 29) + 1))::TIMESTAMP - ((generate_series % 50) || 'years')::INTERVAL as birthdate,
+           (SELECT firstname from firstnames WHERE id = generate_series % (SELECT MAX(id) + 1 FROM firstnames)) as firstname,
+           (SELECT lastname from lastnames WHERE id = generate_series % (SELECT MAX(id) + 1 FROM lastnames)) as lastname,
+           ((1980 + generate_series % 37) || '-' || ((generate_series % 12) + 1) || '-' || ((generate_series % 29) + 1))::TIMESTAMP as hire_date
+    FROM generate_series(1, 101);
+
+DROP TABLE firstnames, lastnames;
+
+INSERT INTO departments(dept_no, dept_name) VALUES
+    ('0001', 'Marketing'),
+    ('0002', 'Sales'),
+    ('0003', 'Legal'),
+    ('0004', 'Java Development'),
+    ('0005', 'Database Administration'),
+    ('0006', 'Strategic Momentum');
+
+INSERT INTO dept_manager
+    SELECT '0001',
+        emp_no,
+        hire_date + ('3 years')::INTERVAL,
+        hire_date + (18 || ' years')::INTERVAL + ('3 years')::INTERVAL
+    FROM (SELECT * FROM employees WHERE date_part('year', hire_date) <= (SELECT MIN(date_part('year', hire_date)) FROM employees) ORDER BY emp_no LIMIT 1) AS oldest_employees;
+
+INSERT INTO dept_manager
+    SELECT '0002',
+        emp_no,
+        hire_date + ('3 years')::INTERVAL,
+        null
+    FROM (SELECT * FROM employees WHERE date_part('year', hire_date) <= (SELECT MIN(date_part('year', hire_date)) FROM employees) ORDER BY ((emp_no + 37) % 100) LIMIT 1) AS oldest_employees;
+
+INSERT INTO dept_manager
+    SELECT '0003',
+        emp_no,
+        hire_date + ('3 years')::INTERVAL,
+        hire_date + (18 || ' years')::INTERVAL + ('3 years')::INTERVAL
+    FROM (SELECT * FROM employees WHERE date_part('year', hire_date) = (SELECT MIN(date_part('year', hire_date)) + 1 FROM employees) ORDER BY ((emp_no + 73) % 100) LIMIT 1) AS oldest_employees;
+
+INSERT INTO dept_manager
+    SELECT '0004',
+        emp_no,
+        hire_date + ('3 years')::INTERVAL,
+        hire_date + (18 || ' years')::INTERVAL + ('3 years')::INTERVAL
+    FROM (SELECT * FROM employees WHERE date_part('year', hire_date) = (SELECT MIN(date_part('year', hire_date)) + 1 FROM employees) ORDER BY (emp_no) LIMIT 1) AS oldest_employees;
+
+INSERT INTO dept_manager
+    SELECT '0005',
+        emp_no,
+        hire_date + ('3 years')::INTERVAL,
+        null
+    FROM (SELECT * FROM employees WHERE date_part('year', hire_date) = (SELECT MIN(date_part('year', hire_date)) + 2 FROM employees) ORDER BY (emp_no) LIMIT 1) AS oldest_employees;
+
+INSERT INTO dept_manager
+    SELECT '0006',
+        emp_no,
+        hire_date + ('3 years')::INTERVAL,
+        hire_date + (18 || ' years')::INTERVAL + ('3 years')::INTERVAL
+    FROM (SELECT * FROM employees WHERE date_part('year', hire_date) = (SELECT MIN(date_part('year', hire_date)) + 3 FROM employees) ORDER BY (emp_no) LIMIT 1) AS oldest_employees;
+
+INSERT INTO dept_manager(dept_no, emp_no, from_date, to_date) VALUES
+    ('0001', 1, '2005-10-5', null);
+
+INSERT INTO dept_manager(dept_no, emp_no, from_date, to_date) VALUES
+    ('0004', 19, '2003-2-28', '2015-12-31');
